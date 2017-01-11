0a73f428a33a1dc8e838024d0099f15706a81ac0
Bwooks <abiyedulla@gmail.com>
2017-01-10 23:53:17 -0500

    Adding testing specifically for redux store

--- voting-server/dist/src/store.js
+++ voting-server/dist/src/store.js
@@ -1,5 +1,15 @@
+"use strict";
+
+var _redux = require("redux");
+
+var _reducer = require("./reducer");
+
+var _reducer2 = _interopRequireDefault(_reducer);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 /**
  * Created by Owner on 1/10/2017.
  */
-"use strict";
+var store = (0, _redux.createStore)(_reducer2.default);
 //# sourceMappingURL=store.js.map
--- voting-server/src/store.js
+++ voting-server/src/store.js
@@ -1,3 +1,7 @@
 /**
  * Created by Owner on 1/10/2017.
  */
+import {createStore} from "redux";
+import reducer from "./reducer";
+
+const store = createStore(reducer);
