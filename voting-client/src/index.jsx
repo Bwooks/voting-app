6b66e890ead4f9ce6e61cd3f1589e7a123246a35
Brooks Dulla <brooksdulla@gmail.com>
2017-01-17 15:16:41 -0500

    Basic functionality of redux store implemented and tested. Reducers unit tested.

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -4,18 +4,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {Router,Route,hashHistory} from "react-router";
+import {createStore} from "redux";
 
 import Voting from "./components/Voting";
 import App from "./components/App";
 import Results from "./components/Results";
-
+import reducer from "./reducer";
 const routes = <Route component={App}>
         <Route path="/" component={Voting} />
         <Route path="/results" component={Results} />
     </Route>;
 
 
-
+const store = createStore(reducer);
+store.dispatch({
+    type:"SET_STATE",
+    vote:{
+        pair:["Rush Hour","Rush Hour 2"],
+        tally:{
+            "Rush Hour":3,
+            "Rush Hour 2":2
+        }
+    }
+});
 ReactDOM.render(
     <Router history={hashHistory}>{routes}</Router>,
     document.getElementById("app")
--- voting-client/test/reducer_spec.jsx
+++ voting-client/test/reducer_spec.jsx
@@ -1,4 +1,82 @@
 import {Map,List,fromJS} from "immutable";
-import {expect} from "chai";
+import chai,{expect} from "chai";
 import reducer from "../src/reducer";
+import chaiImmutable from 'chai-immutable';
+chai.use(chaiImmutable);
 
+describe("Reducer",()=>{
+    it('handles SET_STATE', () => {
+        const initialState = Map();
+        const action = {
+            type: 'SET_STATE',
+            state: Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later'),
+                    tally: Map({Trainspotting: 1})
+                })
+            })
+        };
+        const nextState = reducer(initialState, action);
+
+        expect(nextState).to.equal(fromJS({
+            vote: {
+                pair: ['Trainspotting', '28 Days Later'],
+                tally: {"Trainspotting": 1}
+            }
+        }));
+    });
+
+    it("handles SET_STATE when a state isn't provided",()=>{
+        const initialState = undefined;
+        const action = Map({
+            type: "SET_STATE",
+            state: Map({
+                vote: Map({
+                    pair: ["Ender's Game", "Dune"],
+                    tally: Map({
+                        "Ender's Game": 2,
+                        "Dune": 4
+                    })
+                })
+            })
+        });
+
+        const nextState = reducer(initialState,action);
+        expect(nextState).to.equal(fromJS({
+           vote:{
+               pair:["Ender's Game","Dune"],
+               tally:{
+                   "Ender's Game":2,
+                   "Dune":4
+               }
+           }
+        }));
+    });
+
+    it("handles SET_STATE when a plain JS object is the action payload",()=>{
+       const initialState = Map();
+       const action = {
+           type:"SET_STATE",
+           state:{
+               vote:{
+                   pair:["Band of Brothers","Zero Dark Thirty"],
+                   tally:{
+                       "Band of Brothers":8,
+                       "Zero Dark Thirty":3
+                   }
+               }
+           }
+       };
+       const nextState = reducer(initialState,action);
+       expect(nextState).to.equal(fromJS({
+           vote:Map({
+               pair:["Band of Brothers","Zero Dark Thirty"],
+               tally:Map({
+                   "Band of Brothers":8,
+                   "Zero Dark Thirty":3
+               })
+           })
+       }));
+    });
+
+});
